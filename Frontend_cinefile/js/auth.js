74857ea3fe4486cba163aec79612a1c0ccf712d4
AugustoUMC <11211100981@alunos.umc.br>
2025-10-27 23:32:05 -0300

    Delete Frontend_cinefile directory

--- Frontend_cinefile/js/auth.js
+++
@@ -1,5 +0,0 @@
-// js/auth.js – API simples p/ outras páginas
-window.AUTH = {
-  isLogged() { return !!(window.API && API.Auth && API.Auth.has && API.Auth.has()); },
-  logout() { if (window.API && API.Auth) API.Auth.clear(); }
-};
